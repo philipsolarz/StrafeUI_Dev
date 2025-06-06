cb43cf2f11d3971f5ae52c7a8ec5718c3b2b7eda
Philip Solarz <philipsolarz@outlook.com>
2025-06-06 20:27:19 +0200

    asdf

---
+++ Plugins/StrafeUI/Source/StrafeUI/Private/UI/S_UI_TextButton.cpp
@@ -0,0 +1,15 @@
+// S_UI_TextButton.cpp
+
+#include "UI/S_UI_TextButton.h"
+
+void US_UI_TextButton::NativePreConstruct()
+{
+    Super::NativePreConstruct();
+
+    // If our Text_Label widget exists, update its text with the value
+    // from our exposed ButtonText property.
+    if (Text_Label)
+    {
+        Text_Label->SetText(ButtonText);
+    }
+}
---
+++ Plugins/StrafeUI/Source/StrafeUI/Public/UI/S_UI_TextButton.h
@@ -0,0 +1,31 @@
+// S_UI_TextButton.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonButtonBase.h"
+#include "CommonTextBlock.h"
+#include "S_UI_TextButton.generated.h"
+
+/**
+ * A reusable button that exposes a Text property to be set on instances.
+ */
+UCLASS()
+class STRAFEUI_API US_UI_TextButton : public UCommonButtonBase
+{
+    GENERATED_BODY()
+
+public:
+    // This is the C++ equivalent of an "Instance Editable" variable.
+    // It will show up in the Details panel of any WBP_TextButton instance.
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Button Text")
+    FText ButtonText;
+
+protected:
+    // This function runs in the editor, allowing us to see our changes live.
+    virtual void NativePreConstruct() override;
+
+    // This binds the UMG TextBlock widget named "Text_Label" to this C++ pointer.
+    UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+    TObjectPtr<UCommonTextBlock> Text_Label;
+};
